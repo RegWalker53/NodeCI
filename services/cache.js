c592c24fc33e2c9055374014097f31896ce60a4e
RegWalker53 <Reginald.t.Walker@gmail.com>
2018-07-15 13:06:34 -0500

    Working Redis hashkey and delete

---
+++ services/cache.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+const util = require('util');
+
+client.hget = util.promisify(client.hget);
+const exec = mongoose.Query.prototype.exec; //saving original exec
+
+mongoose.Query.prototype.cache = function(options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+  return this; // makes the function chainable
+};
+
+mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    console.log('reading from Mongo -- no caching');
+    return exec.apply(this, arguments);
+  }
+  console.log('useCache: ', this.useCache);
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+
+  // Check if we have a value for 'key' in redis
+  const cacheValue = await client.hget(this.hashKey, key); // empty means no key
+
+  // If we do, return data from cache
+  if (cacheValue) {
+    console.log('cacheValue: ', cacheValue);
+    const doc = JSON.parse(cacheValue);
+    // console.log('doc:', doc);
+    // Convert the JSON result to a single model or array of models
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d)) // creates array of models
+      : new this.model(doc); // returns a single model
+  }
+
+  console.log('reading data from mongo w/key --', this.hashKey + ':' + key);
+
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments); // executes the original
+  console.log('------');
+  console.log('------');
+  console.log('------');
+  console.log('preparing to write data to redis');
+  console.log('------');
+  console.log('------');
+  client.hset(this.hashKey, key, JSON.stringify(result));
+  // console.log('result: ', result);
+  return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
+};
