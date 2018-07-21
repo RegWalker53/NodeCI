110b9f2a2808e66ff21605a91ebd0d0c41dab05d
RegWalker53 <Reginald.t.Walker@gmail.com>
2018-07-20 19:13:11 -0500

    finished login test and fake login

--- tests/header.test.js
+++ tests/header.test.js
@@ -17,10 +17,10 @@ beforeEach(async () => {
 
 afterEach(async () => {
   // performs post test clean up
-  await browser.close;
+  await browser.close();
 });
 
-test('The header ahs the correct text', async () => {
+test('The header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
 });
@@ -31,3 +31,38 @@ test('clicking login starts oauth flow', async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 });
+
+test.('When signed in shows logout button', async () => {
+  // get user is from database users data
+  const id = '5b33a2146f2bc587bc3467ff';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+      passport: {
+        user: id
+      }
+  };
+  // Fake login using our sessionObject to creating session and session.sig
+  const sessionString = Buffer.from(JSON.stringify(sessionObject))
+                        .toString('base64');
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+
+  await page.goto('localhost:3000');
+  /*
+  console.log('session: ',sessionString);
+  console.log('signature: ', sig);
+*/
+
+
+});
+
+
+
